21833e1822fbba8297bc5c86affbc24a00187d25
Felipe Bueno <felipebueno1229@hotmail.com>
2018-10-06 21:28:06 -0500

    3D Aerial Warfare model made in Unity.

---
+++ Assets/Scripts/MissileMotion.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class MissileMotion : MonoBehaviour {      public float translationpeed;    // Use this for initialization  void Start () {             }       // Update is called once per frame  void Update () {         this.transform.Translate(Vector3.forward);
+
+        this.transform.Translate(0, 0, translationpeed);
+
+    }      void OnCollisionEnter(Collision collision){         Debug.Log("Colision entre dos colliders");          //Destruir misil         GameObject.Destroy(this.gameObject);          //Destruir enemigo         GameObject.Destroy(collision.collider.gameObject);     } } 
